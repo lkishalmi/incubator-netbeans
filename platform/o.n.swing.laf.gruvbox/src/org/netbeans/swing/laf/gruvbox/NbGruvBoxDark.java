0e322961003fc203bba309dcb83a1c02e80203b9
Laszlo Kishalmi <laszlo.kishalmi@gmail.com>
2019-11-29 10:44:46 -0800

    Adding initial gruvbox theme and LaF support

---
+++ platform/o.n.swing.laf.gruvbox/src/org/netbeans/swing/laf/gruvbox/NbGruvBoxDark.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.netbeans.swing.laf.gruvbox;
+
+import com.formdev.flatlaf.FlatLaf;
+import javax.swing.UIDefaults;
+
+/**
+ *
+ * @author lkishalmi
+ */
+public class NbGruvBoxDark extends FlatLaf {
+
+    @Override
+    public boolean isDark() {
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return "Gruvbox Dark";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Gruvbox Dark (experimental)";
+    }
+    
+    @Override
+    public UIDefaults getDefaults() {
+        UIDefaults ret = super.getDefaults();
+        ret.put( "nb.dark.theme", Boolean.TRUE );
+        return ret;
+    }
+
+}
